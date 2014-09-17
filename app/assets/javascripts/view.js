f4e52cb1a97a3043d4b9e1db5c5c9ae6bb4c3a1b
AviFoxi <frosenox@gmail.com>
2014-09-16 21:41:17 -0400

    move view functions in prototype block

--- app/assets/javascripts/view.js
+++ app/assets/javascripts/view.js
@@ -1,9 +1,76 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+function View(elements) {
+	this.playToggle = elements.playToggle;
+	this.tempoSlide = elements.tempoSlide;
+	this.volKnob = elements.volKnob;
+}
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+View.prototype = {
+	addSound : function(e)	{
+		var form = e.currentTarget;
+		var newRowId = form.firstElementChild.value;
+		console.log(newRowId);
+		appendNewRow(newRowId);
+
+		for (var i = 0; i < 4; i++){
+			appendNewBeat(newRowId, i);
+		}
+	},
+	appendNewRow : function(rowId)	{
+		var row = document.createElement('tr');
+		row.id = rowId;
+		var heading = document.createElement('th')
+		heading.innerText = rowId;
+		row.appendChild(heading);
+		$('table').append(row);
+	}, 
+	appendNewBeat : function(rowId, index) {
+	
+		var td = document.createElement('td');
+		var beatCell = document.createElement('div');
+		beatCell.classList.add('beatCell');
+		
+		var input = document.createElement('input');
+		input.type = 'checkbox';
+		var beatId = rowId + index;
+		input.id = beatId
+		
+		beatCell.appendChild(input);
+
+		var label = document.createElement('label');
+		label.htmlFor = beatId;
+
+		beatCell.appendChild(label);
+		td.appendChild(beatCell);
+
+		$('#' + rowId).append(td);
+
+	},
+	removeRow : function(rowId) {
+		$("#" + rowId).remove();
+	}
+}
+
+
+
+
+
+
+
+function toggleMetroLight(){
+	var metro = document.getElementById('metronome');
+	var onOff = metro.checked;
+	metro.checked = !onOff;
+
+}
+
+
+
+
+
+
+$(document).ready(function() {
+  
 
   $('#newRow').submit(function(e){
   	e.preventDefault();
@@ -16,68 +83,3 @@ $(document).ready(function() {
 });
 
 
-function addSound(e){
-	var form = e.currentTarget;
-	var newRowId = form.firstElementChild.value;
-	console.log(newRowId);
-	appendNewRow(newRowId);
-
-	for (var i = 0; i < 4; i++){
-		appendNewBeat(newRowId, i);
-	}
-}
-
-
-function View(elements) {
-
-}
-
-function toggleMetroLight(){
-	var metro = document.getElementById('metronome');
-	var onOff = metro.checked;
-	metro.checked = !onOff;
-
-}
-
-
-// WORKING!
-
-function appendNewRow(rowId){
-	var row = document.createElement('tr');
-	row.id = rowId;
-	var heading = document.createElement('th')
-	heading.innerText = rowId;
-	row.appendChild(heading);
-	$('table').append(row);
-}
-
-// WORKING!
-
-function removeRow(rowId) {
-	$("#" + rowId).remove();
-}
-
-
-// WORKING!
-function appendNewBeat(rowId, index) {
-	
-	var td = document.createElement('td');
-	var beatCell = document.createElement('div');
-	beatCell.classList.add('beatCell');
-	
-	var input = document.createElement('input');
-	input.type = 'checkbox';
-	var beatId = rowId + index;
-	input.id = beatId
-	
-	beatCell.appendChild(input);
-
-	var label = document.createElement('label');
-	label.htmlFor = beatId;
-
-	beatCell.appendChild(label);
-	td.appendChild(beatCell);
-
-	$('#' + rowId).append(td);
-
-}
