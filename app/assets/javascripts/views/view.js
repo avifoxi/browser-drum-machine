eecfa1a9928c5fd313ac346a98aee35c98100080
AviFoxi <frosenox@gmail.com>
2014-09-18 21:12:39 -0400

    extract beat building to beat builder object in views

--- app/assets/javascripts/views/view.js
+++ app/assets/javascripts/views/view.js
@@ -31,66 +31,70 @@ function View(model, elements) {
 
 View.prototype = {
 	buildGrid : function() {
-		var _this = this;
-		var rowData = _this.model.template.samplePatterns;
-		var table = $('table');
-
-		for (var i = 0; i < rowData.length; i++) {
-			var fullRow = _this.addSound(rowData[i])
-			table.append(fullRow);
-		}
-		$('section').append(table); 
-
-	},
-	addSound : function( rowData )	{
+		var patterns = Model.template.samplePatterns;
 		
-		var name = rowData.name;
-		var ptrn = rowData.pattern;
-		var row = this.createNewRow(name);
+		beatBuilder.buildGrid(patterns);
+
+		// var _this = this;
+		// var rowData = _this.model.template.samplePatterns;
+		// var table = $('table');
+
+		// for (var i = 0; i < rowData.length; i++) {
+		// 	var fullRow = _this.addSound(rowData[i])
+		// 	table.append(fullRow);
+		// }
+		// $('section').append(table); 
+
+	} 
+	// addSound : function( rowData )	{
 		
-		for (var i = 0; i < ptrn.length; i++){
-			this.appendNewBeat(row, name, i, ptrn[i]);
-		}
-		return row;
-	},
-	createNewRow : function(name)	{
-		var row = document.createElement('tr');
-		row.id = name;
-		var heading = document.createElement('th')
-		heading.innerText = name;
-		row.appendChild(heading);
-		return row;
-	}, 
-	appendNewBeat : function(row, name, index, playBool) {
+	// 	var name = rowData.name;
+	// 	var ptrn = rowData.pattern;
+	// 	var row = this.createNewRow(name);
+		
+	// 	for (var i = 0; i < ptrn.length; i++){
+	// 		this.appendNewBeat(row, name, i, ptrn[i]);
+	// 	}
+	// 	return row;
+	// },
+	// createNewRow : function(name)	{
+	// 	var row = document.createElement('tr');
+	// 	row.id = name;
+	// 	var heading = document.createElement('th')
+	// 	heading.innerText = name;
+	// 	row.appendChild(heading);
+	// 	return row;
+	// }, 
+	// appendNewBeat : function(row, name, index, playBool) {
 	
-		var td = document.createElement('td');
-		var beatCell = document.createElement('div');
-		beatCell.classList.add('beatCell');
+	// 	var td = document.createElement('td');
+	// 	var beatCell = document.createElement('div');
+	// 	beatCell.classList.add('beatCell');
 		
-		var input = document.createElement('input');
-		input.type = 'checkbox';
+	// 	var input = document.createElement('input');
+	// 	input.type = 'checkbox';
 
-		var beatId = name + '-' + index;
-		input.id = beatId
+	// 	var beatId = name + '-' + index;
+	// 	input.id = beatId
 		
-		beatCell.appendChild(input);
+	// 	beatCell.appendChild(input);
 
-		var label = document.createElement('label');
-		label.htmlFor = beatId;
+	// 	var label = document.createElement('label');
+	// 	label.htmlFor = beatId;
 
-		beatCell.appendChild(label);
-		td.appendChild(beatCell);
+	// 	beatCell.appendChild(label);
+	// 	td.appendChild(beatCell);
 
-		row.appendChild(td);
+	// 	row.appendChild(td);
 		
-		if (playBool) {
-			input.checked = true;
-		}
+	// 	if (playBool) {
+	// 		input.checked = true;
+	// 	}
 
-	},
-	removeRow : function(rowId) {
-		$("#" + rowId).remove();
-	}
+	// },
+	// removeRow : function(rowId) {
+	// 	$("#" + rowId).remove();
+	// }
 }
 
 
