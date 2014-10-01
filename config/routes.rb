082f8919e44103978122bc2403f5803484856edb
AviFoxi <frosenox@gmail.com>
2014-10-01 10:41:42 -0400

    loading s3 audio.. finally

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'templates#index'
 
+  get 'templates/:id' => 'templates#show'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
