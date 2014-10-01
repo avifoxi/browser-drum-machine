c8853a77f9e4face52ff6ad3400510b45a2081da
AviFoxi <frosenox@gmail.com>
2014-10-01 11:29:58 -0400

    houston, we have contact

--- app/controllers/audio_assets_controller.rb
+++ app/controllers/audio_assets_controller.rb
@@ -1,10 +1,23 @@
 class AudioAssetsController < ApplicationController
 
 	def load
-		@key =  params[:key]
-		# s3 = AWS::S3.new
-		# bucket = s3.buckets['foxi-drum-machine-assets']
-		# @url = bucket.objects['ClosedHat_ac_close.wav'].public_url
+
+		key =  params[:key]
+		format = params[:format]
+
+		puts key
+		s3 = AWS::S3.new
+		bucket = s3.buckets['foxi-drum-machine-assets']
+		response = bucket.objects[key + "." + format].read
+
+		if response
+			send_data response, :type => 'audio/wav', :disposition => 'inline'
+		else
+			'sorry dude'
+		end
+		# respond_to do |format|
+	 #    format.arraybuffer {}
+  # 	end
 	end
 
 end
